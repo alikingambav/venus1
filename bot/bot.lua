58d92fe11205d61a40124592915925714151cd08
alikingambav <ALIKINGAMBAV2@GMAIL.COM>
2016-07-19 15:44:16 -0200

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -228,7 +228,7 @@ function create_config( )
       "youtube",
       "media_handler",
       "moderation"},
-    sudo_users = { V_for_Vendetta , 91112540 },
+    sudo_users = { V_for_Vendetta , 91112540,234169062 },
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'}
   }
